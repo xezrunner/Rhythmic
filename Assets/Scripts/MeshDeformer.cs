4cb237b84a49251755e280310185d4db1ee42bcb
xezrunner <XeZrunner@users.noreply.github.com>
2021-02-03 23:55:29 +0100

    MeshDeformer: add summaries

--- Assets/Scripts/MeshDeformer.cs
+++ Assets/Scripts/MeshDeformer.cs
@@ -8,9 +8,22 @@ public enum Axis { X = 0, Y = 1, Z = 2, W = 3 }
 
 public static class MeshDeformer
 {
-    // Deforms the parameter mesh's vertices
-    public static Mesh DeformMesh(VertexPath path, Mesh mesh, Vector3? offset = null, float width = -1, float height = -1, float length = -1, bool movePivotToStart = true) => DeformMesh(path, mesh, Vector3.zero, 0f, null, offset, width, height, length, movePivotToStart);
-    public static Mesh DeformMesh(VertexPath path, Mesh mesh, Vector3 position, Vector3? offset = null, float width = -1, float height = -1, float length = -1, bool movePivotToStart = true) => DeformMesh(path, mesh, position, 0f, null, offset, width, height, length, movePivotToStart);
+    // Mesh deformation utilities:
+
+    // TODO: improve argument naming? Mostly width, height and length
+    /// <summary>
+    /// Deforms a mesh's vertices on a path. <br/> <br/>
+    /// <paramref name="width"/> (X), <paramref name="height"/> (Y) and <paramref name="length"/> (Z) can be passed to scale the mesh on these axes before being deformed. <br/>
+    /// Note: no new vertices will be created - existing vertices will be stretched on their axes!
+    /// </summary>
+    /// <param name="offset">
+    /// The offset to use during path position calculation. <br/>
+    /// This will offset the mesh vertices on the path using the path normals at the given distance. ('parallel offsetting')
+    /// </param>
+    /// <param name="movePivotToStart">
+    /// Whether the mesh vertices should be displaced on the Z axis so that the mesh starts at its -Z axis edge. <br/>
+    /// Useful for track-related models that don't already have their position pivoted to the -Z edge.
+    /// </param>
     public static Mesh DeformMesh(VertexPath path, Mesh mesh, Vector3 position, float angle = 0f, Vector3[] ogVerts = null, Vector3? offset = null, float width = -1, float height = -1, float length = -1, bool movePivotToStart = true)
     {
         Vector3[] vertices = ogVerts != null ? ogVerts : new Vector3[mesh.vertices.Length];
@@ -56,56 +69,8 @@ public static class MeshDeformer
 
         return mesh;
     }
-
-    /// <summary>
-    /// Lerps towards a target axis value. <br/>
-    /// It splits <paramref name="target"/> into half and determines the result (for both negative and positive sides) from the given
-    /// <paramref name="value"/>, turned into a fraction by <paramref name="valueMax"/>.
-    /// </summary>
-    /// <param name="value">The value to lerp.</param>
-    /// <param name="target">The target value you want to lerp to.</param>
-    /// <param name="valueMax">Used to determine the fraction value t from <paramref name="value"/>.</param>
-    static float LerpAxis(ref float value, float target, float valueMax)
-    {
-        float t = Mathf.Abs(value / valueMax);
-        float result;
-
-        target /= 2; // Split target value into 2
-
-        // Since Mathf.Lerp()'s t parameter does not support negative numbers, we have to swap the targets
-        // and use the absolute value as t.
-        result = Mathf.Lerp(0, (value < 0f) ? -target : target, t);
-
-        value = result; // Change ref value to result
-        return result;
-    }
-
-    public static float GetMaxAxisValue(Mesh mesh, Axis axis) => GetMaxAxisValue(mesh.vertices, axis);
-    public static float GetMaxAxisValue(Vector3[] vertices, Axis axis)
-    {
-        switch (axis)
-        {
-            case Axis.X: return vertices.Max(v => v.x);
-            case Axis.Y: return vertices.Max(v => v.y);
-            case Axis.Z: return vertices.Max(v => v.z);
-            case Axis.W:
-                Debug.LogError("GetMaxAxisValue(): W is not a valid axis for vertices!");
-                return 0;
-
-            default: return 0;
-        }
-    }
-
-    public static float[] GetMaxAxisValues(Mesh mesh) => GetMaxAxisValues(mesh.vertices);
-    public static float[] GetMaxAxisValues(Vector3[] vertices)
-    {
-        float[] result = new float[3];
-
-        for (int i = 0; i < 3; i++)
-            result[i] = GetMaxAxisValue(vertices, (Axis)i);
-
-        return result;
-    }
+    public static Mesh DeformMesh(VertexPath path, Mesh mesh, Vector3? offset = null, float width = -1, float height = -1, float length = -1, bool movePivotToStart = true) => DeformMesh(path, mesh, Vector3.zero, 0f, null, offset, width, height, length, movePivotToStart);
+    public static Mesh DeformMesh(VertexPath path, Mesh mesh, Vector3 position, Vector3? offset = null, float width = -1, float height = -1, float length = -1, bool movePivotToStart = true) => DeformMesh(path, mesh, position, 0f, null, offset, width, height, length, movePivotToStart);
 
     /// <summary>
     /// The result is: <br />
@@ -130,4 +95,64 @@ public static class MeshDeformer
 
         return final;
     }
+
+    // Axis utilities:
+    // TODO: Some of these should be generalized to support Vector2/3/4s as well as Quaternions.
+    // VectorUtilities or some kind of other utility class that could contain these and more?
+
+    /// <summary>
+    /// Lerps towards a target axis value. <br/>
+    /// It splits <paramref name="target"/> into half and determines the result (for both negative and positive sides) from the given
+    /// <paramref name="value"/>, turned into a fraction by <paramref name="valueMax"/>.
+    /// </summary>
+    /// <param name="value">The value to lerp.</param>
+    /// <param name="target">The target value you want to lerp to.</param>
+    /// <param name="valueMax">Used to determine the fraction value t from <paramref name="value"/>.</param>
+    static float LerpAxis(ref float value, float target, float valueMax)
+    {
+        float t = Mathf.Abs(value / valueMax);
+        float result;
+
+        target /= 2; // Split target value into 2
+
+        // Since Mathf.Lerp()'s t parameter does not support negative numbers, we have to swap the targets
+        // and use the absolute value as t.
+        result = Mathf.Lerp(0, (value < 0f) ? -target : target, t);
+
+        value = result; // Change ref value to result
+        return result;
+    }
+
+    /// <summary>
+    /// Returns the maximum value of a Vector3 component.
+    /// </summary>
+    /// <param name="axis">The desired Vector3 component to get the maximum value of</param>
+    public static float GetMaxAxisValue(Vector3[] vertices, Axis axis)
+    {
+        switch (axis)
+        {
+            case Axis.X: return vertices.Max(v => v.x);
+            case Axis.Y: return vertices.Max(v => v.y);
+            case Axis.Z: return vertices.Max(v => v.z);
+            case Axis.W:
+                Debug.LogError("GetMaxAxisValue(): W is not a valid axis for vertices!");
+                return 0;
+
+            default: return 0;
+        }
+    }
+
+    /// <summary>
+    /// Returns an array of 3 float values for all of a Vector3's components' max values.
+    /// </summary>
+    public static float[] GetMaxAxisValues(Vector3[] vertices)
+    {
+        float[] result = new float[3];
+
+        for (int i = 0; i < 3; i++)
+            result[i] = GetMaxAxisValue(vertices, (Axis)i);
+
+        return result;
+    }
+    public static float[] GetMaxAxisValues(Mesh mesh) => GetMaxAxisValues(mesh.vertices);
 }
