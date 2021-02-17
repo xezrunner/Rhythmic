6696f663dbd271769deec7b3c0d0e4aa97f15918
xezrunner <XeZrunner@users.noreply.github.com>
2021-02-17 21:06:38 +0100

    Colors: Add Hex to Color

--- Assets/Scripts/Colors.cs
+++ Assets/Scripts/Colors.cs
@@ -1,9 +1,37 @@
-﻿using UnityEngine;
+﻿using System.Globalization;
+using UnityEngine;
 
 public class Colors : MonoBehaviour
 {
-    public static Color ConvertColor(Color color)
+    // Editor:
+    [Header("For: ConvertToFloatColor()")]
+    public Vector4 Color = new Vector4(0, 0, 0, 255);
+    [Header("For: ConvertHexToColor()")]
+    public string Input;
+
+    // Functionality:
+    public static Color ConvertToFloatColor(Color color)
     {
         return new Color(color.r / 255, color.g / 255, color.b / 255, color.a / 255);
+    }
+
+    public static Color ConvertHexToColor(string hex, bool convertToFloat = true)
+    {
+        // Remove # from start, if exists
+        if (hex[0] == '#') hex = hex.Substring(1);
+
+        bool hasAlpha = hex.Length == 8;
+        int[] rgbaColors = new int[4];
+
+        for (int i = 0, c = !hasAlpha ? 0 : 2; c < hex.Length; c += 2, i++)
+            rgbaColors[i] = int.Parse(hex.Substring(c, 2), NumberStyles.AllowHexSpecifier);
+
+        // Add alpha if exists : 255
+        rgbaColors[3] = hasAlpha ? int.Parse(hex.Substring(0, 2), NumberStyles.AllowHexSpecifier) : 255;
+
+        Color finalColor = new Color(rgbaColors[0], rgbaColors[1], rgbaColors[2], rgbaColors[3]);
+
+        if (convertToFloat) return ConvertToFloatColor(finalColor);
+        else return finalColor;
     }
 }
---
+++ Assets/Scripts/Editor/ColorsEditor.cs
@@ -0,0 +1,24 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(Colors))]
+class ColorsEditor : Editor
+{
+    Colors main;
+    void Awake() => main = (Colors)target;
+
+    public Color? output = null;
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        GUILayout.Label($"Output: {output}");
+
+        if (GUILayout.Button("ConvertToFloatColor()"))
+            output = Colors.ConvertToFloatColor(new Color(main.Color.x, main.Color.y, main.Color.z, main.Color.w));
+
+        if (GUILayout.Button("ConvertHexToColor()"))
+            output = Colors.ConvertHexToColor(main.Input);
+    }
+}
