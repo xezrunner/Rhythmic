4d4a5802cdab84402309bf3663769d9fabc6398f
xezrunner <XeZrunner@users.noreply.github.com>
2021-02-17 22:11:50 +0100

    Colors: add StringExtensions -> inline 'rich string' color
    string.AddColor(text, color)

--- Assets/Scripts/Colors.cs
+++ Assets/Scripts/Colors.cs
@@ -2,6 +2,14 @@
 using System.Reflection;
 using UnityEngine;
 
+// TODO: move somewhere else?
+public static class StringExtensions
+{
+    /// Credit: https://forum.unity.com/threads/change-color-of-a-single-word.538706/#post-6819410
+    public static string AddColor(this string text, Color col) => $"<color={ColorHexFromUnityColor(col)}>{text}</color>";
+    public static string ColorHexFromUnityColor(this Color unityColor) => $"#{ColorUtility.ToHtmlStringRGBA(unityColor)}";
+}
+
 public class Colors : MonoBehaviour
 {
     // Editor:
