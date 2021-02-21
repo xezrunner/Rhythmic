c04f8399e34b6c66b0d88f4a1b4c30ce89dd7fbe
xezrunner <XeZrunner@users.noreply.github.com>
2021-02-21 22:55:30 +0100

    Move StringExtensions out of Colors

---
+++ Assets/Scripts/StringExtensions.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public static class StringExtensions
+{
+    // Colors:
+    /// Credit: https://forum.unity.com/threads/change-color-of-a-single-word.538706/#post-6819410
+    public static string AddColor(this string text, Color col) => $"<color={ColorHexFromUnityColor(col)}>{text}</color>";
+    public static string ColorHexFromUnityColor(this Color unityColor) => $"#{ColorUtility.ToHtmlStringRGBA(unityColor)}";
+}
