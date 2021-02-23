079810b74a848651b4407ab9baf06febb50a3ebe
xezrunner <XeZrunner@users.noreply.github.com>
2021-02-23 20:58:38 +0100

    StringExt/AddColor(): allow taking in RGBA params

--- Assets/Scripts/StringExtensions.cs
+++ Assets/Scripts/StringExtensions.cs
@@ -4,7 +4,7 @@ public static class StringExtensions
 {
     // Colors:
     /// Credit: https://forum.unity.com/threads/change-color-of-a-single-word.538706/#post-6819410
-    public static string AddColor(this string text, Color col) => $"<color={ColorHexFromUnityColor(col)}>{text}</color>";
+    public static string AddColor(this string text, float r, float g, float b, float? alpha = null) => AddColor(text, new Color(r, g, b, alpha));
     public static string AddColor(this string text, Color color, float? alpha = null)
     {
         if (alpha.HasValue) color.a = alpha.Value; // Set alpha if required
