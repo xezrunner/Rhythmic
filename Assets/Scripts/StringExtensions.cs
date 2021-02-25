baa882154928808a096037111fa09286fc3db494
xezrunner <XeZrunner@users.noreply.github.com>
2021-02-25 20:25:22 +0100

    StringExts/AddColor(): overload with alpha only

--- Assets/Scripts/StringExtensions.cs
+++ Assets/Scripts/StringExtensions.cs
@@ -4,6 +4,7 @@ public static class StringExtensions
 {
     // Colors:
     /// Credit: https://forum.unity.com/threads/change-color-of-a-single-word.538706/#post-6819410
+    public static string AddColor(this string text, float alpha) => AddColor(text, new Color(1, 1, 1), alpha);
     public static string AddColor(this string text, float r, float g, float b, float? alpha = null) => AddColor(text, new Color(r, g, b), alpha);
     public static string AddColor(this string text, Color color, float? alpha = null)
     {
