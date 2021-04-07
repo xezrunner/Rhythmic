b4009f84804ee96a59249e1aaaf68925d8552b71
xezrunner <XeZrunner@users.noreply.github.com>
2021-04-07 18:34:41 +0200

    Switch versioning to YYYYMMDD-HHMM

--- Assets/Scripts/Version.cs
+++ Assets/Scripts/Version.cs
@@ -1,4 +1,14 @@
-﻿public static class Version
+﻿using System;
+
+public static class Version
 {
-    public static string build_string = "Build 20210326-03";
+    /// <summary>Returns the build string in YYYYMMDD-HHMM format.</summary>
+    public static string build_string
+    {
+        get
+        {
+            return $"Build {DateTime.Now.Date.Year}{DateTime.Now.Month.ToString("00")}{DateTime.Now.Day.ToString("00")}" +
+                           $"-{DateTime.Now.Hour.ToString("00")}{DateTime.Now.Minute.ToString("00")}";
+        }
+    }
 }
