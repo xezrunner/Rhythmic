151420fb2c6df8633bf354899f1600cc680808b3
Sebastian Kassai <8061077+xezrunner@users.noreply.github.com>
2021-11-14 15:02:59 +0100

    AMP: Midi reading

---
+++ Rhythmic/Assets/Rhythmic/AMP_2016/AMP_MidiFile.cs
@@ -0,0 +1,46 @@
+﻿using System.IO;
+using NAudio.Midi;
+using static Logger;
+
+// Fields:
+
+public partial class AMP_MidiFile
+{
+    public int bpm;
+}
+
+// Functionality:
+
+public partial class AMP_MidiFile
+{
+    public AMP_MidiFile(string path) { ReadMIDIFromPath(path); }
+
+    public void ReadMIDIFromPath(string path)
+    {
+        if (!File.Exists(path) && LogE("File does not exist: '%'".TM(this), path)) 
+            return;
+
+        byte[] bytes = File.ReadAllBytes(path);
+        Stream stream = new MemoryStream(bytes);
+        MidiFile midi = new MidiFile(stream, false); // TODO: Should 'strictChecking' be true?
+
+        bpm = find_bpm_from_midi(midi);
+    }
+
+    int find_bpm_from_midi(MidiFile midi)
+    {
+        int event_count = midi.Events[0].Count;
+        for (int i = 0; i < event_count; ++i)
+        {
+            MidiEvent ev = midi.Events[0][i];
+            if (ev.GetType() != typeof(TempoEvent)) continue;
+
+            TempoEvent tempo_ev = (TempoEvent)ev;
+            int bpm = 60000 / (tempo_ev.MicrosecondsPerQuarterNote / 1000); // convert (us->ms) to minutes
+            return bpm;
+        }
+
+        LogW("BPM not found!".TM(this));
+        return 0;
+    }
+}
--- Rhythmic/Assets/Rhythmic/Test.cs
+++ Rhythmic/Assets/Rhythmic/Test.cs
@@ -7,6 +7,9 @@ public class Test : MonoBehaviour
     void Start()
     {
         AMP_MoggSong a = new AMP_MoggSong(@"G:\amp_ps3\songs\allthetime\allthetime.moggsong");
-        Log("mogg_path: %  midi_path: %" , a.mogg_path, a.midi_path);
+        Log("MoggSong CHECK: mogg_path: %  midi_path: %" , a.mogg_path, a.midi_path);
+
+        AMP_MidiFile b = new AMP_MidiFile(@"G:\amp_ps3\songs\allthetime\allthetime.mid");
+        Log("MidiFile CHECK: bpm: %", b.bpm);
     }
 }
